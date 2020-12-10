6d0b98cfd70e4d0b6cfa2e9d0b2ceae837c7c21e
ANDRERAIMUNDO <9000andre@gmail.com>
2020-12-10 11:35:21 -0300

    Criando Site Aplicação MVC AspNetCore3 usando Template padrão

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BelaRosa_MVC_AspNetCore3_4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
